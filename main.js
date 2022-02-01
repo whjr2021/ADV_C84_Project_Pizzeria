491d97f06b224828603dc15a1e43b72a5b59a25b
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2022-02-01 15:15:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+menu_list_array = ["Veg Margherita Pizza",
+                    "Chicken Tandoori Pizza",
+                    "Veg Supreme Pizza",
+                    "Paneer Tikka Pizza",
+                    "Deluxe Veggie Pizza",
+                    "Veg Extravaganza Pizza"];
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+}
